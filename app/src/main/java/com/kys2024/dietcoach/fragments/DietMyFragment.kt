505d8a9de20478dff6eb638ce4be9715de64622d
im62 <yeongseunggim62@gmail.com>
2024-04-05 09:07:05 +0900

    BMI 분석, 기초대사량 계산 마이페이지 작업

--- app/src/main/java/com/kys2024/dietcoach/fragments/DietMyFragment.kt
+++ app/src/main/java/com/kys2024/dietcoach/fragments/DietMyFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kys2024.dietcoach.databinding.FragmentDietMyBinding
+import kotlin.math.roundToInt
 
 class DietMyFragment:Fragment() {
 
@@ -25,28 +26,39 @@ class DietMyFragment:Fragment() {
 
 
 
+
     }
 
 
     private fun clickBtn(){
 
 
-        var heightText=binding.inputKey.editText!!.text.toString()
-        var weightText=binding.inputWeight.editText!!.text.toString()
+        var ageText=binding.inputAge.editText!!.text.toString().toIntOrNull()
+        var heightText=binding.inputKey.editText!!.text.toString().toDoubleOrNull()
+        var weightText=binding.inputWeight.editText!!.text.toString().toDoubleOrNull()
 
-        if (weightText.isNotEmpty()&&heightText.isNotEmpty()){
+        if (weightText!=null && heightText!=null && ageText!=null){
+            val isMale = binding.maleRadioButton.isChecked
+            if (isMale){
+                val isMale1=66.47+(13.75*weightText.toInt()) + (5*heightText.toInt()) - (6.76*ageText.toInt()).toInt()
+                binding.tvBmr.text="기초대사량: ${isMale1.toInt()} Kcal"
+            }else{
+                val famle1=655.1+(9.56*weightText.toInt()) + (1.85*heightText.toInt()) - (4.68*ageText.toInt()).toInt()
+                binding.tvBmr.text="기초대사량: ${famle1.toInt()} Kcal"
+            }
+        }
+
+        if (weightText!=null && heightText!=null){
             val weight=weightText.toDouble()
             val height=heightText.toDouble()
 
             val bmi = calculateBMI(weight,height)
             val obesityGrade = getObesityGrade(bmi)
 
-            binding.tvBmi.text="BMI: $bmi\n 비만도 등급:$obesityGrade"
-
-
+            binding.tvBmi.text="BMI: ${bmi.toInt()}\n비만도 등급:$obesityGrade"
         }
-
     }
+
 
     private fun calculateBMI(weight:Double,height:Double):Double{
         val heightInMeters = height/100
@@ -60,5 +72,4 @@ class DietMyFragment:Fragment() {
             else->"비만"
         }
     }
-
 }
