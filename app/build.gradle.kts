c20ab1ab80a1cb1626d0cd85798dfa7c03bc682f
chlguss97 <chlguss97@gmail.com>
2024-04-08 10:41:01 +0900

    오류수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation(libs.material.calendarview2)
 
 
 
