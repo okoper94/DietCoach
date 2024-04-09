4e7d66288a7330980e85adb5bfc4ecbb5b66a5fd
im62 <yeongseunggim62@gmail.com>
2024-04-09 17:22:54 +0900

    Camera,Profile

--- app/src/main/java/com/kys2024/dietcoach/fragments/DietMyFragment.kt
+++ app/src/main/java/com/kys2024/dietcoach/fragments/DietMyFragment.kt
@@ -1,11 +1,20 @@
 package com.kys2024.dietcoach.fragments
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
 import com.kys2024.dietcoach.activity.MyInformationActivity
 import com.kys2024.dietcoach.databinding.FragmentDietMyBinding
 import kotlin.math.roundToInt
@@ -25,6 +34,8 @@ class DietMyFragment:Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.tvBmi.setOnClickListener { clickBmi() }
+        binding.ivEditing.setOnClickListener { clickPhoto() }
+
 
 
 
@@ -32,7 +43,26 @@ class DietMyFragment:Fragment() {
 
 
     }
-    private fun clickBmi(){
+
+
+    private fun clickPhoto(){ //버튼클릭시 내 앨범에서 사진선택해서 프로필 변경
+        val intent = if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU) Intent(MediaStore.ACTION_PICK_IMAGES)else Intent(Intent.ACTION_OPEN_DOCUMENT).setType("image/*")
+        resultLauncher.launch(intent)
+    }
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        val uri:Uri?=it.data?.data
+        if (uri!=null){
+            Glide.with(this).load(uri).apply(RequestOptions().transform(CircleCrop())).into(binding.ivProfile)
+
+
+        }
+    }
+
+
+
+
+
+    private fun clickBmi(){  //버튼 클릭시 MyInformationActivity 로 이동
         startActivity(Intent(requireContext(),MyInformationActivity::class.java))
     }
 
