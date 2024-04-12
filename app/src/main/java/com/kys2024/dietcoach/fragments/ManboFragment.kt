f191464c798d23df693ce7b1761f65910c4f860b
chlguss97 <chlguss97@gmail.com>
2024-04-12 14:15:39 +0900

    오류수정

--- app/src/main/java/com/kys2024/dietcoach/fragments/ManboFragment.kt
+++ app/src/main/java/com/kys2024/dietcoach/fragments/ManboFragment.kt
@@ -14,8 +14,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
