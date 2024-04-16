2d4ed813ce34eddab08b5ab503bb4c891d227d46
chlguss97 <chlguss97@gmail.com>
2024-04-16 09:47:41 +0900

    0415

--- app/src/main/java/com/kys2024/dietcoach/fragments/ManboFragment.kt
+++ app/src/main/java/com/kys2024/dietcoach/fragments/ManboFragment.kt
@@ -1,44 +1,32 @@
 package com.kys2024.dietcoach.fragments
 
-import android.Manifest.*
-import android.Manifest.permission.*
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.content.pm.PackageManager.*
-import android.graphics.drawable.Drawable
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.kys2024.dietcoach.R
 import com.kys2024.dietcoach.databinding.FragmentManboBinding
 
-class ManboFragment : Fragment() {
+class ManboFragment : Fragment(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private var stepCountSensor: Sensor? = null
     private lateinit var binding: FragmentManboBinding
 
-    private val stepCountListener = object : SensorEventListener {
-        override fun onSensorChanged(event: SensorEvent) {
-            if (event.sensor.type == Sensor.TYPE_STEP_COUNTER) {
-                val stepCount = event.values[0].toInt()
-                activity?.runOnUiThread {
-                    binding.tvStepbox.text = "걸음 수: $stepCount"
-                }
-            }
-        }
-
-        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-            // 센서 정확도 변경 이벤트 처리
-        }
+    companion object {
+        private const val PERMISSION_ACTIVITY_RECOGNITION_REQUEST_CODE = 100
     }
 
     override fun onCreateView(
@@ -47,46 +35,47 @@ class ManboFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentManboBinding.inflate(inflater, container, false)
-
-        // ImageView에 Glide 이미지 로딩
-        Glide.with(requireContext())
-            .load(R.drawable.move)
-            .into(binding.ivMove)
-
-        // SensorManager 초기화
-        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
-        // 만보기 센서 가져오기
-        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        // Activity Recognition 권한 체크 및 요청
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                permission.ACTIVITY_RECOGNITION
-            ) == PackageManager.PERMISSION_DENIED
-        ) {
-            requestPermissions(arrayOf(permission.ACTIVITY_RECOGNITION), 0)
-        }
-
         return binding.root
     }
 
-    override fun onResume() {
-        super.onResume()
-        // 센서 리스너 등록
-        stepCountSensor?.let {
-            sensorManager.registerListener(
-                stepCountListener,
-                it,
-                SensorManager.SENSOR_DELAY_NORMAL
-            )
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        // 만보 센서 체크
+        if (stepCountSensor == null) {
+            Toast.makeText(requireContext(), "해당 기기에는 만보 센서가 없습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            // 권한 체크
+            if (!isActivityRecognitionPermissionGranted()) {
+                // 권한 요청
+                requestActivityRecognitionPermission()
+            } else {
+                // 권한이 이미 허용된 경우 센서 리스너 등록
+                registerSensorListener()
+            }
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-        // 센서 리스너 해제
-        sensorManager.unregisterListener(stepCountListener)
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun isActivityRecognitionPermissionGranted(): Boolean {
+        // Activity Recognition 권한 체크
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACTIVITY_RECOGNITION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun requestActivityRecognitionPermission() {
+        // Activity Recognition 권한 요청
+        requestPermissions(
+            arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+            PERMISSION_ACTIVITY_RECOGNITION_REQUEST_CODE
+        )
     }
 
     override fun onRequestPermissionsResult(
@@ -95,19 +84,50 @@ class ManboFragment : Fragment() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 0 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // 퍼미션 허용되면 센서 리스너 다시 등록
-            stepCountSensor?.let {
-                sensorManager.registerListener(
-                    stepCountListener,
-                    it,
-                    SensorManager.SENSOR_DELAY_NORMAL
-                )
+        if (requestCode == PERMISSION_ACTIVITY_RECOGNITION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한 허용 시 센서 리스너 등록
+                registerSensorListener()
+            } else {
+                // 권한이 거부된 경우 토스트 메시지로 안내
+                Toast.makeText(
+                    requireContext(),
+                    "만보 센서를 사용하기 위해 권한이 필요합니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
+
+    private fun registerSensorListener() {
+        // 센서 리스너 등록
+        stepCountSensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // 센서 정확도 변경 이벤트 처리
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        // 만보 센서 데이터 처리
+        event?.let {
+            if (it.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
+                val stepCount = it.values[0].toInt()
+                updateStepCount(stepCount)
+            }
+        }
+    }
+
+    private fun updateStepCount(stepCount: Int) {
+        // UI 업데이트
+        binding.tvStepbox.text = "총 $stepCount 걸음 수"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // 센서 리스너 해제
+        sensorManager.unregisterListener(this)
+    }
 }
-
-
-
-
