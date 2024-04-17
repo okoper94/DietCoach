3ed395a3b1bd26011171ad0d0d6cedf1319ece69
Dreamtreeme <k99702@gmail.com>
2024-04-17 13:56:35 +0900

    db작업

--- app/src/main/java/com/kys2024/dietcoach/G.kt
+++ app/src/main/java/com/kys2024/dietcoach/G.kt
@@ -1,12 +1,15 @@
 package com.kys2024.dietcoach
 
 
+import com.kys2024.dietcoach.data.BoardData
 import com.kys2024.dietcoach.data.UserAccount
 
 
 class G {
     companion object {
         var userAccount: UserAccount? = null
+
+        var DB: MutableList<BoardData> = mutableListOf()
 
     }
 }
--- app/src/main/java/com/kys2024/dietcoach/data/LoadBoardData.kt
+++ app/src/main/java/com/kys2024/dietcoach/data/LoadBoardData.kt
@@ -1,9 +1,11 @@
 package com.kys2024.dietcoach.data
 
 data class LoadBoardData (
-    var userid:String,
-    var profileimg:String,
-    var no:Int,
-    var image:String,
-    var msg:String
+   var DB: MutableList<BoardData>
     )
+
+data class BoardData(var userid:String,
+                     var profileimg:String,
+                     var no:Int,
+                     var image:String,
+                     var msg:String)
